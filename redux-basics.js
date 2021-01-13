7b0307a9942f32870b4a27e3a95a36a89c7dd288
ranjeet2003 <ranjeetgautam13032@gmail.com>
2021-01-13 16:11:32 +0530

    upto lecture 9

--- redux-basics.js
+++ redux-basics.js
@@ -5,8 +5,33 @@ const initialState = {
   counter: 0,
 };
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "IDD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    };
+  }
   return state;
 };
+
 // Store
+
 const store = createStore(rootReducer);
 console.log(store.getState());
+
+// Dispatching action
+
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
+
+// Subscription
+store.subscribe(() => {
+  console.log("[Subscription]", store.getState());
+});
